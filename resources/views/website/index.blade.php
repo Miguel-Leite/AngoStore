20aca2a9c9b8af70ce8be67f6f1aeabe1833c86f
Miguel Leite <miguelleite200leite@gmail.com>
2022-04-17 10:39:56 +0100

    time in realtime

--- resources/views/website/index.blade.php
+++ resources/views/website/index.blade.php
@@ -81,31 +81,37 @@
 					<div class="row">
 						<div class="products-tabs">
 							<div id="#" class="tab-pane active"> 
-								<div class="products-slick" data-nav="#slick-nav-1">
-									@foreach ($products as $product)
-										<!-- product -->
-										<div class="product">
-											<div class="product-img">
-												<img src="{{ url("storage/$product->image_default") }}" alt="{{ $product->image_default }}">
-												<div class="product-label">
-													<span class="new">Novo</span>
+								@if (count($products) > 0)	
+									<div class="products-slick" data-nav="#slick-nav-1">
+										@foreach ($products as $product)
+											<!-- product -->
+											<div class="product">
+												<div class="product-img">
+													<img src="{{ url("storage/$product->image_default") }}" alt="{{ $product->image_default }}">
+													<div class="product-label">
+														<span class="new">Novo</span>
+													</div>
+												</div>
+												<div class="product-body">
+													<p class="product-category">Categoria: {{ $product->category }}</p>
+													<h3 class="product-name"><a href="{{ route('website.product',$product->product) }}">{{ $product->product }}</a></h3>
+													<h4 class="product-price">AKZ {{formatPrice($product->price)}} <del class="product-old-price">AKZ {{lastPrice($product->price)}}</del></h4>
+													<div class="product-btns">
+														<button onclick="viewDetailProduct('{{ route('website.product',$product->product) }}')" class="quick-view"><i class="fa fa-eye"></i><span class="tooltipp">Ver detalhes</span></button>
+													</div>
+												</div>
+												<div class="add-to-cart">
+													<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> Comprar </button>
 												</div>
 											</div>
-											<div class="product-body">
-												<p class="product-category">Categoria: {{ $product->category }}</p>
-												<h3 class="product-name"><a href="{{ route('website.product',$product->product) }}">{{ $product->product }}</a></h3>
-												<h4 class="product-price">AKZ {{formatPrice($product->price)}} <del class="product-old-price">AKZ {{lastPrice($product->price)}}</del></h4>
-												<div class="product-btns">
-													<button onclick="viewDetailProduct('{{ route('website.product',$product->product) }}')" class="quick-view"><i class="fa fa-eye"></i><span class="tooltipp">Ver detalhes</span></button>
-												</div>
-											</div>
-											<div class="add-to-cart">
-												<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> Comprar </button>
-											</div>
-										</div>
-										<!-- /product -->
-									@endforeach
-								</div>
+											<!-- /product -->
+										@endforeach
+									</div>
+								@else:
+									<div class="alert alert-info text-center">
+										<b>Lamentamos informar,</b> que não foi registrado nenhum produto na nossa loja. Por favor tente mas tarde
+									</div>
+								@endif
 							</div>
 						</div>
 					</div>
@@ -130,32 +136,32 @@
 						<ul class="hot-deal-countdown">
 							<li>
 								<div>
-									<h3>02</h3>
+									<h3 class="day">00</h3>
 									<span>Dia(s)</span>
 								</div>
 							</li>
 							<li>
 								<div>
-									<h3>10</h3>
+									<h3 class="hour">00</h3>
 									<span>Hora(s)</span>
 								</div>
 							</li>
 							<li>
 								<div>
-									<h3>34</h3>
+									<h3 class="min">00</h3>
 									<span>Minuto(s)</span>
 								</div>
 							</li>
 							<li>
 								<div>
-									<h3>60</h3>
+									<h3 class="second">00</h3>
 									<span>Segundo(s)</span>
 								</div>
 							</li>
 						</ul>
 						<h2 class="text-uppercase">Os produtos da semana</h2>
 						<p>Novas colecções com 50% de desconto</p>
-						<a class="primary-btn cta-btn" href="#">Comprar Agora</a>
+						<a class="primary-btn cta-btn" href="{{ route('website.products') }}">Comprar Agora</a>
 					</div>
 				</div>
 			</div>
@@ -191,31 +197,37 @@
 						<div class="products-tabs">
 							<!-- tab -->
 							<div id="#" class="tab-pane active"> 
-								<div class="products-slick" data-nav="#slick-nav-1">
-									@foreach ($products as $product)
-										<!-- product -->
-										<div class="product">
-											<div class="product-img">
-												<img src="{{ url("storage/$product->image_default") }}" alt="{{ $product->image_default }}">
-												<div class="product-label">
-													<span class="new">Novo</span>
+								@if (count($products) > 0)	
+									<div class="products-slick" data-nav="#slick-nav-1">
+										@foreach ($products as $product)
+											<!-- product -->
+											<div class="product">
+												<div class="product-img">
+													<img src="{{ url("storage/$product->image_default") }}" alt="{{ $product->image_default }}">
+													<div class="product-label">
+														<span class="new">Novo</span>
+													</div>
+												</div>
+												<div class="product-body">
+													<p class="product-category">Categoria: {{ $product->category }}</p>
+													<h3 class="product-name"><a href="{{ route('website.product',$product->product) }}">{{ $product->product }}</a></h3>
+													<h4 class="product-price">AKZ {{formatPrice($product->price)}} <del class="product-old-price">AKZ {{lastPrice($product->price)}}</del></h4>
+													<div class="product-btns">
+														<button onclick="viewDetailProduct('{{ route('website.product',$product->product) }}')" class="quick-view"><i class="fa fa-eye"></i><span class="tooltipp">Ver detalhes</span></button>
+													</div>
+												</div>
+												<div class="add-to-cart">
+													<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> Comprar </button>
 												</div>
 											</div>
-											<div class="product-body">
-												<p class="product-category">Categoria: {{ $product->category }}</p>
-												<h3 class="product-name"><a href="{{ route('website.product',$product->product) }}">{{ $product->product }}</a></h3>
-												<h4 class="product-price">AKZ {{formatPrice($product->price)}} <del class="product-old-price">AKZ {{lastPrice($product->price)}}</del></h4>
-												<div class="product-btns">
-													<button onclick="viewDetailProduct('{{ route('website.product',$product->product) }}')" class="quick-view"><i class="fa fa-eye"></i><span class="tooltipp">Ver detalhes</span></button>
-												</div>
-											</div>
-											<div class="add-to-cart">
-												<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> Comprar </button>
-											</div>
-										</div>
-										<!-- /product -->
-									@endforeach
-								</div>
+											<!-- /product -->
+										@endforeach
+									</div>
+								@else
+									<div class="alert alert-info text-center">
+										<b>Lamentamos informar,</b> que não foi registrado nenhum produto na nossa loja. Por favor tente mas tarde
+									</div>
+								@endif
 							</div>
 							<!-- /tab -->
 						</div>
@@ -239,5 +251,18 @@
 			location.assign(url);
 		}
 	}
+	setInterval(() => {
+		let date = new Date()
+		let day = date.getDate() < 10? '0'+date.getDate():date.getDate();
+		let hour = date.getHours() < 10? '0'+date.getHours():date.getHours();
+		let min = date.getMinutes() < 10? '0'+date.getMinutes():date.getMinutes();
+		let sec = date.getSeconds() < 10? '0'+date.getSeconds():date.getSeconds();
+		document.querySelector('.day').innerHTML=`${day}`
+		document.querySelector('.hour').innerHTML=`${hour}`
+		document.querySelector('.min').innerHTML=`${min}`
+		document.querySelector('.second').innerHTML=`${sec}`
+		// document.querySelector('.min').innerHTML=`${}`
+		// document.querySelector('.second').innerHTML=`${}`
+	}, 1000);
 </script>
 @endsection
