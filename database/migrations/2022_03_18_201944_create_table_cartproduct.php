74b541409be838d37df7a71a3f478dbe68c7bde2
Miguel Leite <miguelleite200leite@gmail.com>
2022-04-20 21:45:12 +0100

    alter entity cartsproducts

--- database/migrations/2022_03_18_201944_create_table_cartproduct.php
+++ database/migrations/2022_03_18_201944_create_table_cartproduct.php
@@ -17,7 +17,7 @@ class CreateTableCartproduct extends Migration
             $table->id();
             $table->foreignId("carts_id")->constrained()->cascadeOnDelete();
             $table->foreignId("products_id")->constrained()->cascadeOnDelete();
-            $table->dateTime("dtremoved");
+            $table->dateTime("dtremoved")->nullable();
             $table->timestamps();
         });
     }
