6659ac7d25c8f119d4e1c8a4b54603a3bffc0233
Miguel Leite <miguelleite200leite@gmail.com>
2022-05-02 21:14:18 +0100

    ✅ register client

--- public/website/js/auth.js
+++ public/website/js/auth.js
@@ -3,7 +3,7 @@ import { Api } from './services.js';
 document.forms[0].addEventListener('submit',async (e)=> {
     e.preventDefault();
     const fd = new FormData(e.currentTarget);
-    const { data, status } = await Api.post('account/login',fd);
+    const { data, status } = await Api.post(document.forms[0].action,fd);
     if (data.success) {
         iziToast.success({
             title: 'Sucesso!',
@@ -11,7 +11,7 @@ document.forms[0].addEventListener('submit',async (e)=> {
             position: 'topRight'
           });
           setTimeout(()=>{
-            location.assign("conta/login")
+            location.assign("conta/perfil")
          },3000)
     } else {
         iziToast.error({
@@ -20,4 +20,24 @@ document.forms[0].addEventListener('submit',async (e)=> {
             position: 'topRight'
           });
     }
+},false)
+
+document.forms[1].addEventListener('submit',async (e)=> {
+  e.preventDefault();
+  const fd = new FormData(e.currentTarget);
+  const { data, status } = await Api.post('account/register',fd);
+  if (data.success) {
+      iziToast.success({
+          title: 'Sucesso!',
+          message: data.message,
+          position: 'topRight'
+        });
+        document.forms[1].reset();
+  } else {
+      iziToast.error({
+          title: 'Falha na autenticação!',
+          message: data.message,
+          position: 'topRight'
+        });
+  }
 },false)
