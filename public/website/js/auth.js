7bd40d830b33bcd194cc4733eb04cbf745a9bc65
Miguel Leite <miguelleite200leite@gmail.com>
2022-05-02 20:41:07 +0100

    🚀 authentication-send data to app

---
+++ public/website/js/auth.js
@@ -0,0 +1,25 @@
+import { Api } from './services.js';
+document.querySelector('login-client').addEventListener('submit',authentication,false);
+
+async function authentication(e) {
+    e.preventDefault();
+
+    const fd = new FormData(e.currentTarget);
+    const { data, status } = await Api.post('conta/login',fd);
+    if (data.success) {
+        iziToast.success({
+            title: 'Sucesso!',
+            message: data.message,
+            position: 'topRight'
+          });
+          setTimeout(()=>{
+            location.assign("conta/login")
+         },3000)
+    } else {
+        iziToast.error({
+            title: 'Falha na autenticação!',
+            message: data.message,
+            position: 'topRight'
+          });
+    }
+}
