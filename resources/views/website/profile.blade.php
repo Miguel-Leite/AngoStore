fd9032e9a10279addb367b2eedb194855580d667
Miguel Leite <miguelleite200leite@gmail.com>
2022-05-02 19:36:10 +0100

    👌 create view profile

---
+++ resources/views/website/profile.blade.php
@@ -0,0 +1,17 @@
+@extends('website.layouts.profile')
+@section('title','Perfil')
+
+@section('content')
+    <section class="profile">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-xl-4 col-md-6 col-xs-12">
+                    1
+                </div>
+                <div class="col-lg-8 col-xl-8 col-md-6 col-xs-12">
+                    2
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
