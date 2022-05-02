0a1c4d895c1ce64ef48782359d81da80de87283b
Miguel Leite <miguelleite200leite@gmail.com>
2022-05-02 20:25:56 +0100

    ✅ add method loginClient

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -39,6 +39,32 @@ class AuthController extends Controller
         }
     }
 
+    public function loginClient (Request $request)
+    {
+        if (Auth::attempt(['login' => $request->login,'password'=>$request->password])) {
+            $login = auth()->user()->login;
+            if (auth()->user()->inadmin > 0) {
+                // Session::flush();
+                    Auth::logout();
+                    return json_encode([
+                    "success"=>false,
+                    "message" => "$login não tens permissão de acessar essa página."
+                ]);
+            }
+            return json_encode([
+                "success"=>true,
+                "error"=>false,
+                "message"=> "Login efectuado com successo. Seja bem-vindo $login!"
+            ]);
+        } else {
+            return json_encode([
+                "success"=>false,
+                "error"=>true,
+                "message" => "Login ou senha estão incorretos."
+            ]);
+        }
+    }
+
     public function logout() {
         Session::flush();
         
